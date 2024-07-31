bf5eebe93b619a03c7c5597251e03b0f8839f98b
Max <48150531+mashtakovmm@users.noreply.github.com>
2024-07-31 13:25:47 +0300

    add better responses

--- backend/controllers/tasks.ts
+++ backend/controllers/tasks.ts
@@ -4,18 +4,24 @@ import Task from '../models/Task'
 export const getAllTasks = async (req: Request, res: Response) => {
     try {
         const tasks = await Task.find();
-        res.status(201).json(tasks);
+        res.status(201).json({
+            success: true, data: {
+                tasks, nbHits: tasks.length
+            }
+        });
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json({ success: false, error: err });
     }
 }
 
 export const postTask = async (req: Request, res: Response) => {
     try {
         const task = await Task.create(req.body);
-        res.status(201).json(task);
+        res.status(201).json({
+            success: true, task: task
+        });
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json({ success: false, error: err });
     }
 }
 
@@ -23,11 +29,11 @@ export const delTask = async (req: Request, res: Response) => {
     try {
         const task = await Task.findByIdAndDelete(req.params.id);
         if (!task) {
-            res.status(500).send("No task found")
+            res.status(500).json({ success: false, error: `No task with id ${req.params.id} found` })
         }
-        res.status(201).send("Task deleted");
+        res.status(201).json({ success: true });
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json({ success: false, error: err });
     }
 }
 
@@ -38,9 +44,9 @@ export const changeTask = async (req: Request, res: Response) => {
                 new: true,
                 runValidators: true
             });
-        res.status(201).json(task);
+        res.status(201).json({ success: true, task: task });
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json({ success: false, error: err });
     }
 }
 
