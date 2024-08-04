04b37105803b2ed9778703af8920c3935cde7750
Max <48150531+mashtakovmm@users.noreply.github.com>
2024-08-04 14:34:14 +0300

    add border color to tasks based on their deadline date

--- frontend/src/components/TodoItem/TodoItem.tsx
+++ frontend/src/components/TodoItem/TodoItem.tsx
@@ -3,13 +3,14 @@ import Button from "../Button/Button";
 import { MdDeleteOutline } from "react-icons/md";
 import { useDeleteTaskMutation, usePatchTaskMutation } from "../../services/tasks";
 import "./TodoItem.css"
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 
 const TodoItem: React.FC<TodoItemType> = ({ _id, name, description, completed, deadline }) => {
 
     const [deleteTask] = useDeleteTaskMutation();
     const [patchTask] = usePatchTaskMutation();
+    const [deadlineColor, setDeadlineColor] = useState('');
 
     function formatDate(ISODate?: string) {
         if (ISODate) {
@@ -28,8 +29,31 @@ const TodoItem: React.FC<TodoItemType> = ({ _id, name, description, completed, d
         patchTask({ _id: _id, completed: completed })
     }, [_id])
 
+    useEffect(() => {
+        if (deadline) {
+            const deadlineDate = new Date(deadline)
+            deadlineDate.setHours(0, 0, 0, 0)
+
+            const now = new Date(Date.now())
+            now.setHours(0, 0, 0, 0)
+
+            if (deadlineDate > now) {
+                setDeadlineColor("#39bf4b")
+            } else if (deadlineDate < now) {
+                setDeadlineColor('#ff0f0f')
+            } else {
+                setDeadlineColor('#dbdb44')
+            }
+            console.log(`Deadline: ${deadlineDate}`);
+            console.log(`Now: ${now}`);
+        }
+        else {
+            setDeadlineColor('#f0f0f0')
+        }
+    }, [])
+
     return (
-        <li className={`todo-item ${completed ? 'completed' : ''}`}>
+        <li className={`todo-item ${completed ? 'completed' : ''}`} style={{borderColor:`${deadlineColor}`}}>
             <div className="todo-left" onClick={handleToggleCompleted}>
                 <h2>{`${name}`}</h2>
                 <p>{`${description ? description : 'No description was provided'}`}</p>
